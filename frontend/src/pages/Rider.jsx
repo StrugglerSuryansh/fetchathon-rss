84a84b55337c2056c24c5056449834ad3e74d8d1
Hirdesh Khandelwal <hirdeshkhandelwal58@gmail.com>
2024-11-06 07:50:12 +0530

    fix: added some padding around the rider page.

--- frontend/src/pages/Rider.jsx
+++ frontend/src/pages/Rider.jsx
@@ -4,9 +4,10 @@ import RideList from '../components/RideList';
 
 function Rider() {
   return (
-    <div className="min-h-screen bg-gradient-to-r from-blue-500 to-purple-600">
+    <div className="min-h-screen bg-gradient-to-r from-blue-500 to-purple-600 p-3">
       <h1 className="mb-6 text-3xl font-bold">Find a Ride</h1>
       <RideForm type="request" />
+      <div className='mt-3'></div>
       <RideList />
     </div>
   );
